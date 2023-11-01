0089f786c611737f444d9198aecc298a627dadd4
Kleogis <kleogis@hotmail.com>
2023-10-31 20:26:02 -0700

    add ToDo list app initial commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,33 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Viktoriia Liu on 10/23/23.
+//
+
+import SwiftUI
+
+/*
+ 
+ MVVM Architecture
+ 
+ Model - data point
+ View - UI
+ ViewModel - manager Models for View
+ 
+ */
+@main
+struct TodoListApp: App {
+    
+    @StateObject var listViewModel: ListViewModel = ListViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ListView()
+            }
+            .environmentObject(listViewModel)
+            
+        }
+    }
+}
